c352dd506203384250bce165e38a7f34cb0225cb
linomiltenburg <linomiltenburg@hotmail.com>
2015-11-10 16:14:01 +0100

    week 3

---
+++ Homework/week-3/scraper.py
@@ -0,0 +1,23 @@
+import csv
+#OUTPUT_CSV = 'knmi.csv'
+
+#wr = csv.writer(knmi, quoting=csv.QUOTE_ALL)
+#wr.writerow(OUTPUT_CSV)
+
+
+with open('KNMI_20141231.txt', 'r') as f:
+    content = f.readlines()[12:]
+
+with open("data.csv", "wb") as f:
+    writer = csv.writer(f)
+
+    for row in content:
+        # extract date and change notation
+        date = row.split(",")[1]
+        year = date[:4]
+        month = date[4:6]
+        day = date[6:8]
+        date = year + "/" + month + "/" + day
+        # extract temperature
+        temp = row.split(",")[2].strip()
+        writer.writerow([date, temp])
